4ff6b8910fffc979a633aa18ee4e39286c608a54
Njf092024 <Njf092024@gmail.com>
2025-05-13 09:01:41 +0200

    refactor image with next/image and import it to page.js from next/Image

--- app/page.js
+++ app/page.js
@@ -1,4 +1,17 @@
-import Image from 'image.png';
+import Image from 'next/Image';
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return 
+(<main>
+  <h1>Hello, Next.js!</h1>
+  <Image
+  src="/image.png"
+  alt="image logo"
+  width={300}
+  height={200}
+  priority
+  />
+</main>
+);
+  
 }
